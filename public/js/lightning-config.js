3f829089d2591fbb3e32f3e7e87b1f438c7e60b3
Dave Carroll <djcarroll@gmail.com>
2015-10-13 16:07:02 -0700

    config vars

--- public/js/lightning-config.js
+++ public/js/lightning-config.js
@@ -2,7 +2,7 @@
 	force.init(config);
 };
 
-// Config vars
+// Config vars area
 var config = { 
 	appId: "3MVG9SemV5D80oBfwImbjmCUOooxcQA5IOWhAPpgu5tZTe09L944U1N9rqfHev_RHMAu5BMPvkG7_nKbpV8M2", 
 	loApp: "c:HouseExplorerLOApp",
