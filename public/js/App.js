dfd32b40bd605ed2d442a5cdeb3723c26691c880
Dave Carroll <djcarroll@gmail.com>
2015-10-26 16:23:07 -0700

    ecma6

--- public/js/App.js
+++ public/js/App.js
@@ -49,7 +49,7 @@ export let createComponent = () => {
 };
 
 export let forceLogin = key => {
-	forcejs.login();
+	forcejs.init({ appId:"3MVG9SemV5D80oBfwImbjmCUOooxcQA5IOWhAPpgu5tZTe09L944U1N9rqfHev_RHMAu5BMPvkG7_nKbpV8M2" }).login();
 	forceInit({instanceUrl:"https://d10-dev-ed.salesforce.com" });
 	force.login(function(success) {
 		force.getOauth();
